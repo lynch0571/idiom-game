813156d173c18bfe878f1c92c377d6bab132fab6
fire <lynch0571@gmail.com>
2020-12-19 04:21:42 +0800

    Initialize a springboot project

---
+++ src/test/java/com/lynch/idiomgame/IdiomGameApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lynch.idiomgame;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IdiomGameApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
